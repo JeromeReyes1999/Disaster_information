05e3e9e23dc3ef8700570e6eaa0e4ec6a69dbec0
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-28 14:14:58 +0800

    add relationship with delete dependency for post category and comments

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   validates_presence_of :name
+  has_many :post, dependent: :restrict_with_error
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
   belongs_to :user
-  belongs_to :disaster
-
+  belongs_to :category
+  has_many :comments, dependent: :restrict_with_error
 end
