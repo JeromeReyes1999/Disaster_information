ed928a6161c5a246be057f89ef911205f64a78db
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-28 12:25:32 +0800

    add roles to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def client?
+    role == 'client'
+  end
+
+  def admin?
+    role == 'admin'
+  end
 end
