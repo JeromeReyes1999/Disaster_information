458dc536109c100e4d9371c9cc150bac33fea60e
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-28 20:05:16 +0800

    add relationship between post, regions, cities and provinces

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   validates_presence_of :content
   belongs_to :user
   belongs_to :category
+  belongs_to :region
+  belongs_to :province
+  belongs_to :city
   has_many :comments, dependent: :restrict_with_error
+
 
   before_create :generate_unique_number
 
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,5 @@
 class Province < ApplicationRecord
   has_many :city
+  has_many :posts
   belongs_to :region
 end
