38c327fad9514d141141ada9c76d91bc9c715b64
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-28 14:19:30 +0800

    add routes for comments and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :categories
   end
+
+  resources :posts do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
