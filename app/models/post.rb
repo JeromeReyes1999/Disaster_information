790eaa2b7f2a183760f0cfb99bb1717d2c2f71f0
JeromeReyes1999 <rjerome415@gmail.com>
2022-07-29 13:09:28 +0800

    tried to use another method for unique url

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,14 +10,16 @@ class Post < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  before_create :generate_unique_number, :generate_address
+  before_create  :generate_address
+  after_create :generate_unique_number
 
   def generate_unique_number
-    loop do
-      @short = rand(0..9999).to_s.rjust(4, '0')
-      break unless Post.exists?(short_url: @short)
-    end
-    self.short_url = @short
+    # loop do
+    #   @short = rand(0..9999).to_s.rjust(4, '0')
+    #   break unless Post.exists?(short_url: @short)
+    # end
+    # self.short_url = @short
+    self.update(short_url: self.id.to_s.rjust(4, '0'))  #try to use id instead
   end
 
   def generate_address
